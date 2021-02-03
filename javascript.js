c5817eed2c1a7c92e0cd4fb9ad15d393f29443f7
Siw <siwx0310@stud.kea.dk>
2021-02-03 16:39:20 +0100

    json file lode one server test

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ let closeWin = document.querySelector("#close");
 
 async function start() {
   console.log("start");
-  let jsonFile = "/actors.json";
+  let jsonFile = "../actors.json";
   let jsonData = await fetch(jsonFile);
   actors = await jsonData.json();
   console.log(actors);
